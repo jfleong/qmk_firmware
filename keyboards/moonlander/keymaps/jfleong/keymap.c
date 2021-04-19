31831bc6c16271e251c34ed80a3c2a307d5e0696
J Leong <jason.f.leong@gmail.com>
2021-04-19 09:30:17 -1000

    changes

--- keyboards/moonlander/keymaps/jfleong/keymap.c
+++ keyboards/moonlander/keymaps/jfleong/keymap.c
@@ -109,12 +109,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
-        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,     KC_5,   KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,   _______,          _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   KC_HYPR,          KC_MEH,  KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), LGUI_T(KC_QUOT),
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,                              KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), KC_RSFT,
-        KC_GRV,   _______, _______, KC_LALT, KC_LGUI,          OSL(TTV),         OSL(TTV),         KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT, _______,
-                                    LT(SYMB, KC_ENT), _______, _______,                   KC_ENT,  _______, LT(MDIA, KC_SPC)
+        KC_ESC,   KC_1,    KC_2,     KC_3,    KC_4,     KC_5,   KC_6,             KC_7,       KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+        KC_TAB,   KC_Q,    KC_W,     KC_E,    KC_R,     KC_T,   _______,          LALT(KC_A), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_LCTRL, KC_A,    KC_S,     KC_D,    KC_F,     KC_G,   KC_HYPR,          LALT(KC_V), KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), LGUI_T(KC_QUOT),
+        KC_LSFT,  KC_Z,    KC_X,     KC_C,    KC_V,     KC_B,                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), KC_RSFT,
+        KC_GRV,   _______, KC_LCTRL, KC_LGUI, KC_LALT,          OSL(TTV),         OSL(TTV),            KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______,
+                                     LT(SYMB, KC_ENT), _______, _______,                      KC_ENT,  _______, LT(MDIA, KC_SPC)
     ),
 
     [SYMB] = LAYOUT_moonlander(
@@ -127,8 +127,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [MDIA] = LAYOUT_moonlander(
-        LED_LEVEL,_______,_______, _______, _______, _______, _______,           LCTL(KC_A), _______, _______, _______, _______,  _______, RESET,
-        _______, _______, _______, _______, _______, _______, _______,           LCTL(KC_V), _______, _______, _______, _______,  _______, _______,
+        LED_LEVEL,_______,_______, _______, _______, _______, _______,           LALT(KC_A), _______, _______, _______, _______,  _______, RESET,
+        _______, _______, _______, _______, _______, _______, _______,           LALT(KC_V), _______, _______, _______, _______,  _______, _______,
         _______, _______, _______, _______, _______, _______, _______,           _______,    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
         _______, _______, _______, _______, _______, _______,                                _______, KC_LBRC, KC_RBRC, _______,  _______, _______,
         _______, _______, _______, _______, _______,          _______,           _______,             KC_VOLU, KC_VOLD, KC_MUTE,  _______, _______,
