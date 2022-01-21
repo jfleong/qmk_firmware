3587af42f9f3cad3bc6e9bb7ae4034923c944a00
J Leong <jason.f.leong@gmail.com>
2022-01-21 10:04:23 -1000

    fixes some things

--- keyboards/keebio/iris/keymaps/jfleong/config.h
+++ keyboards/keebio/iris/keymaps/jfleong/config.h
@@ -19,3 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // #define USE_I2C
 #define EE_HANDS
+
+// Enable Light Layers implementation
+#define RGBLIGHT_LAYERS
+// Allow Light Layers to override RGB off configuration
+#define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
