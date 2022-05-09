1d5200c26b3b7cb999ac450519bf63da7e59e959
J Leong <jason.f.leong@gmail.com>
2022-05-09 07:56:53 -1000

    tweaks

--- keyboards/moonlander/keymaps/jfleong/keymap.c
+++ keyboards/moonlander/keymaps/jfleong/keymap.c
@@ -21,8 +21,13 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+#define _ADJUST 3
+
 enum layers {
-    BASE = SAFE_RANGE,  // default layer
+    QWERTY = SAFE_RANGE,  // default layer
     LOWER,  // symbols
     RAISE,  // media keys
     ADJUST,  // twitch tv keys
@@ -30,7 +35,7 @@ enum layers {
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [BASE] = LAYOUT_moonlander(
+    [_QWERTY] = LAYOUT_moonlander(
         KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,     KC_5,   KC_6,             KC_7,       KC_8, KC_9,  KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,   _______,          LALT(KC_A), KC_Y, KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   _______,          LALT(KC_V), KC_H, KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
