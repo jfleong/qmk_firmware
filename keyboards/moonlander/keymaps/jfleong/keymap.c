91d3021502561571097f93558a7e8459ffc750c7
J Leong <jason.f.leong@gmail.com>
2022-05-09 14:09:52 -1000

    adds ralt for moonlander

--- keyboards/moonlander/keymaps/jfleong/keymap.c
+++ keyboards/moonlander/keymaps/jfleong/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,   _______,          LALT(KC_A), KC_Y, KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   _______,          LALT(KC_V), KC_H, KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,                                 KC_N, KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_GRV,   _______, _______, KC_LGUI, LOWER,            KC_LALT,          KC_RALT,          RAISE, _______, _______, _______, LSFT(KC_F22),
+        KC_GRV,   _______, _______, KC_LGUI, LOWER,            KC_LALT,          KC_RALT,          RAISE, KC_RALT, _______, _______, LSFT(KC_F22),
                                              KC_ENT,  KC_SPC,  _______,          _______,  KC_ENT, KC_SPC
     ),
 
